e77ac4de3a5514f1b9ee41369f223b8eb08310f6
Payton Chertude <p.chertude34@gmail.com>
2024-09-10 20:18:52 -0700

    add contact form

---
+++ src/assets/js/file-upload.js
@@ -0,0 +1,9 @@
+document.getElementById('file-upload').addEventListener('change', function(event) {
+  const fileInputHelpText = document.getElementById('file-input-help-text');
+  const file = event.target.files[0];
+  if (file) {
+    fileInputHelpText.innerHTML = file.name;
+  } else {
+    fileInputHelpText.innerHTML = 'Choose a file (Max 15MB)';
+  }
+});
