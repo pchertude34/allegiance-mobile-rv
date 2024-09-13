9e07a75e1c743121df11bf1b35722909f14fc2cd
Payton Chertude <p.chertude34@gmail.com>
2024-09-13 06:53:09 -0700

    meta data

--- src/_data/client.js
+++ src/_data/client.js
@@ -4,17 +4,14 @@ module.exports = {
     "phoneForTel": "971-323-8365",
     "phoneFormatted": "(971) 323-8365",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "lineOne": "2467 Bunker Ridge Rd S",
+        "city": "Salem",
+        "state": "OR",
+        "zip": "97306",
         "country": "US",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "facebook": "https://www.facebook.com/profile.php?id=61552812544327",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
     "domain": "https://allegiancervservices.com"
