2b374ce5aec6e8b68b5d2191ee99fe856f048735
Payton Chertude <p.chertude34@gmail.com>
2024-09-12 20:30:27 -0700

    remove dark mode

--- src/assets/js/file-upload.js
+++ src/assets/js/file-upload.js
@@ -1,9 +1,13 @@
-document.getElementById('file-upload').addEventListener('change', function(event) {
-  const fileInputHelpText = document.getElementById('file-input-help-text');
-  const file = event.target.files[0];
-  if (file) {
-    fileInputHelpText.innerHTML = file.name;
-  } else {
-    fileInputHelpText.innerHTML = 'Choose a file (Max 15MB)';
-  }
-});
+const fileUploadButton = document.getElementById('file-upload');
+
+if (fileUploadButton) {
+  fileUploadButton.addEventListener('change', function (event) {
+    const fileInputHelpText = document.getElementById('file-input-help-text');
+    const file = event.target.files[0];
+    if (file) {
+      fileInputHelpText.innerHTML = file.name;
+    } else {
+      fileInputHelpText.innerHTML = 'Choose a file (Max 15MB)';
+    }
+  });
+}
